09ea4797399f626907647756052b324caac563f1
Mike Lee <mikemunkyu.lee@gmail.com>
2023-03-19 09:56:10 -0400

    Mark the current page in the sidebar

--- src/components/Sidebar.jsx
+++ src/components/Sidebar.jsx
@@ -1,7 +1,24 @@
 import React from "react";
+import { useRouter } from 'next/router';
+
 import Link from "next/link";
 
 const Sidebar = () => {
+  const router = useRouter();
+  const pathname = router.pathname;
+
+  const routes = [
+    {
+      name: "Smart input with RegEx",
+      path: "/smart-regex-input",
+    },
+    {
+      name: "CSS → Tailwind",
+      path: "/css2tw",
+    },
+  ]
+
+
   return (
     <nav className="min-w-[16rem] px-4 h-screen flex flex-col bg-gray-100">
       <div className="py-4 border-b-2 border-b-gray-200 mb-4 font-semibold">
@@ -15,16 +32,17 @@ const Sidebar = () => {
           Tools
         </span>
         <ul className="pl-2 list-none text-blue-600 flex flex-col gap-y-2">
-          <li>
-            <Link href="/smart-regex-input">
-              Smart input with RegEx
+          {
+            routes.map((route) => (
+          <li key={route.path} className="relative">
+            <span className={
+              pathname === route.path ? "absolute left-[-1rem]" : "hidden"
+            }>•</span>
+            <Link href={route.path}>
+              { route.name }
             </Link>
           </li>
-          <li>
-            <Link href="/css2tw">
-            CSS → Tailwind
-            </Link>
-          </li>
+          ))}
         </ul>
       </div>
 
