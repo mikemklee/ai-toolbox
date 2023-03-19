4cfe2ecc498253fc90d54b15f0bc836273f6af57
Mike Lee <mikemunkyu.lee@gmail.com>
2023-03-18 22:41:58 -0400

    Add CSS2TW page

---
+++ src/pages/api/css2tw.ts
@@ -0,0 +1,72 @@
+import { Configuration, OpenAIApi } from "openai";
+
+const configuration = new Configuration({
+  apiKey: process.env.OPENAI_API_KEY,
+});
+const openai = new OpenAIApi(configuration);
+
+async function generate(req, res) {
+  if (!configuration.apiKey) {
+    res.status(500).json({
+      error: {
+        message:
+          "OpenAI API key not configured, please follow instructions in README.md",
+      },
+    });
+    return;
+  }
+
+  const input = req.body.input || "";
+  if (input.trim().length === 0) {
+    res.status(400).json({
+      error: {
+        message: "Please enter a valid input",
+      },
+    });
+    return;
+  }
+
+  const systemMessage = `
+        You are a helpful assistant. You are helping a user, who would like to use Tailwind CSS to style their application.
+        Given a set of raw CSS rules, you will provide the equivalent Tailwind CSS class names.
+        For example, if the user provides the CSS rule: "color: red;" then you will provide the Tailwind CSS class name: "text-red-500".
+    `;
+
+  try {
+    const completion = await openai.createChatCompletion({
+      model: "gpt-3.5-turbo",
+      messages: [
+        { role: "system", content: systemMessage },
+        { role: "user", content: "color: red;" },
+        { role: "assistant", content: "text-red-500" },
+        {
+          role: "user",
+          content:
+            "webkit-transition-property: opacity; transition-property: opacity;",
+        },
+        { role: "assistant", content: "transition-opacity" },
+        { role: "user", content: "border-left-width: 1px;" },
+        { role: "assistant", content: "border-l border-l-1" },
+        { role: "user", content: input },
+      ],
+    });
+
+    res.status(200).json({
+      result: JSON.stringify(completion.data.choices[0].message.content),
+    });
+  } catch (error: any) {
+    if (error.response) {
+      console.error(error.response.status, error.response.data);
+      res.status(error.response.status).json(error.response.data);
+    } else {
+      console.error(`Error with OpenAI API request: ${error.message}`);
+      res.status(500).json({
+        error: {
+          message: "An error occurred during your request.",
+        },
+      });
+    }
+  }
+}
+
+export default generate;
--- src/pages/css2tw/index.jsx
+++ src/pages/css2tw/index.jsx
@@ -1,8 +1,46 @@
 import Head from "next/head";
+import { useState } from "react";
 import styles from "@/styles/Home.module.css";
 import Sidebar from "@/components/Sidebar";
 
 export default function Page() {
+  const [patternInput, setPatternInput] = useState("");
+  const [isLoading, setIsLoading] = useState(false);
+  const [result, setResult] = useState();
+
+  async function onSubmit(event) {
+    event.preventDefault();
+    try {
+      setIsLoading(true);
+      const response = await fetch("/api/css2tw", {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json",
+        },
+        body: JSON.stringify({ input: patternInput }),
+      });
+
+      console.log("response?", response);
+
+      const data = await response.json();
+      if (response.status !== 200) {
+        throw (
+          data.error ||
+          new Error(`Request failed with status ${response.status}`)
+        );
+      }
+
+      setResult(JSON.parse(data.result));
+    } catch (error) {
+      // Consider implementing your own error handling logic here
+      console.error(error);
+      alert(error.message);
+    } finally {
+      setIsLoading(false);
+    }
+  }
+
+
   return (
     <>
       <Head>
@@ -11,9 +49,30 @@ export default function Page() {
 
       <div className={styles.container}>
         <Sidebar />
-        <main className={styles.main}>
-          <h1 className="text-3xl font-bold underline">CSS → Tailwind</h1>
-        </main>
+        <main className="w-80 mx-auto flex flex-col items-center my-auto">
+        <h3 className="my-6">CSS → Tailwind</h3>
+
+        <form onSubmit={onSubmit} className="flex flex-col w-full">
+          <input
+            type="text"
+            name="pattern"
+            className="w-full p-2 rounded bg-gray-800 border border-gray-700"
+            placeholder="Enter CSS rules here"
+            value={patternInput}
+            onChange={(e) => setPatternInput(e.target.value)}
+          />
+          <input
+            type="submit"
+            value="Go"
+            className="w-full p-2 rounded bg-gray-800 mt-2 cursor-pointer opacity-80 hover:opacity-100 transition-opacity"
+          />
+        </form>
+
+        <div className="w-full border-gray-700 text-center py-6 px-2">
+          {isLoading ? "Generating a response..." : result}
+        </div>
+
+      </main>
       </div>
     </>
   );
