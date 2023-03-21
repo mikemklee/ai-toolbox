0a7b14742282cc51fabdd6b4437d0bf8ada09126
Mike Lee <mikemunkyu.lee@gmail.com>
2023-03-20 23:05:01 -0400

    Add color prop to Button component

--- src/components/Button.tsx
+++ src/components/Button.tsx
@@ -4,18 +4,28 @@ interface ButtonProps {
   children: ReactNode;
   onClick: MouseEventHandler<HTMLButtonElement>;
   disabled?: boolean;
+  color: string;
 }
+
+const buttonStyles: Record<ButtonProps["color"], string> = {
+  primary: "bg-teal-600 text-white",
+  warning: "bg-red-500 text-white",
+  disabled: "bg-gray-300 text-gray-400 pointer-events-none",
+};
 
 export default function Button({
   children,
   onClick,
   disabled = false,
+  color = "primary",
 }: ButtonProps) {
+  const styles = buttonStyles[color];
+
   return (
     <button
       onClick={onClick}
       disabled={disabled}
-      className="px-3 py-2 text-white bg-teal-600 rounded text-center cursor-pointer"
+      className={`px-3 py-2 cursor-pointer ${styles} rounded text-center`}
     >
       {children}
     </button>
